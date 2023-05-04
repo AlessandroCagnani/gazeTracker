4c2bba0087925d20bdfd11dc409eed1d9f5e93f7
AlessandroCagnani <alessandro.cagnani@usi.ch>
2023-05-04 14:54:47 +0200

    multiple point calib routine, dummy correction in tracker

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from gaze_estimation.gaze_estimator import GazeEstimator
 import cv2
 import numpy as np
 from typing import Tuple
-import os
-import yaml
+from utils.visualizer import Visualizer
+import json
 
 import argparse
 
@@ -30,6 +30,15 @@ def get_point_on_screen(monitor_mm: Tuple[float, float], monitor_pixels: Tuple[f
 
     return tuple(np.asarray([result_x, result_y]).round().astype(int))
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
 
 # mode 0: calibration, mode 1: mirror, mode 2: tracker
 class Demo:
@@ -39,34 +48,64 @@ class Demo:
         self.camera = Camera("data/camera/camera_params.yaml")
         self.gaze_estimator = GazeEstimator(self.camera, True)
 
+        if not mode == 1:
+            pygame.init()
+            screen = pygame.display.set_mode((1792, 1120), pygame.FULLSCREEN)
+            self.visualizer = Visualizer(screen)
+        else:
+            self.visualizer = Visualizer()
+        self.run()
+
+        #TODO: add flag to see correction / error vectors
+
     def run(self):
         if self.mode == 0:
+            ret = self.calib_routine()
             print("[ CALIBRATION ] name of the user: ")
             name = input()
-            self.run_calibration(name)
-            avg, data = self.get_calib_data(name)
-            print("mean x and y: ", avg)
+
+            for point_ref in ret.keys():
+                ret[point_ref]["mean"] = np.mean(ret[point_ref]["data"], axis=0).tolist()
+
+            calib_data = list()
+            for point_ref in ret.keys():
+                point_coord = point_ref.split("_")
+                point_coord = (int(point_coord[0]), int(point_coord[1]))
+                calib_data.append(dict(point=point_coord, mean=ret[point_ref]["mean"], data=ret[point_ref]["data"]))
+
+            with open(f"data/calib/{name}.json", "w") as f:
+                json.dump(calib_data, f, cls=NpEncoder)
+
+            print("[ CALIBRATION ] Calibration data saved")
         elif self.mode == 1:
             pass
         else:
             self.run_tracker()
 
     def run_tracker(self):
+
         print("[ TRACKER ] starting eye tracker ")
-        pygame.init()
-
-        screen = pygame.display.set_mode((1792, 1120), pygame.FULLSCREEN)
-
         off_x = 0
         off_y = 0
         if self.user_calib is not None:
             print("[ TRACKER ] getting calibration ")
-            avg, data = self.get_calib_data(self.user_calib)
-            off_x = 896 - avg[0]
-            off_y = 560 - avg[1]
+            data = self.get_calib_data(self.user_calib)
+            center = next(obj for obj in data if obj["point"][0] == 896 and obj["point"][1] == 560)
+            off_x = 896 - center["mean"][0]
+            off_y = 560 - center["mean"][1]
+
+        ref_points = [(1792 // 2, 1120 // 2),
+                      (1792 // 5, (1120 // 5) * 2),
+                      (1792 // 5, (1120 // 5) * 3),
+                      (1792 // 5 * 2, 1120 // 5),
+                      (1792 // 5 * 3, 1120 // 5),
+                      (1792 // 5 * 2, 1120 // 5 * 4),
+                      (1792 // 5 * 3, 1120 // 5 * 4),
+                      (1792 // 5 * 4, (1120 // 5) * 2),
+                      (1792 // 5 * 4, (1120 // 5) * 3)]
 
         while True:
-            screen.fill((255, 255, 255))
+            self.visualizer.fill_background((255, 255, 255))
 
             ret, frame = self.camera.get_frame()
             if not ret:
@@ -83,36 +122,15 @@ class Demo:
 
                 # face.center[2] = face.get_distance(self.camera)[1]
 
-                pygame.draw.circle(screen, (0, 255, 0), (0, 0), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792 // 2, 1120 // 2), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792 // 4, 1120 // 4), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792 // 4 * 3, 1120 // 4), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792 // 4, 1120 // 4 * 3), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792 // 4 * 3, 1120 // 4 * 3), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (0, 1120), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792, 0), 10)
-                pygame.draw.circle(screen, (0, 255, 0), (1792, 1120), 10)
+                self.visualizer.draw_ref_points(ref_points)
 
-                plane_norm = np.array([0, 0, 1])
+                estimated_point = self.gaze_to_screen(face)
 
-                d = np.dot(plane_norm, face.gaze_vector)
-
-                face.center[2] = face.center[2] - 0.0009
-
-                if d != 0:
-                    p_point = np.array([0, 0, 0])
-                    t = np.dot(p_point - face.leye.center * 1000, plane_norm) / d
-                    gaze_point = face.leye.center * 1000 + t * face.gaze_vector
-                    print(f"Gaze point: {gaze_point}")
-                    # Check if gaze_point is within the screen boundaries
-                    x, y = get_point_on_screen((340, 220), (1792, 1120), gaze_point)
-                    print(f"Screen coordinates: {x}, {y}")
-                    pygame.draw.circle(screen, (255, 0, 0), (x + off_x, y + off_y), 10)
-                else:
-                    print("Gaze vector is parallel to the screen")
+                corrected_point = (estimated_point[0] + off_x, estimated_point[1] + off_y)
+                self.visualizer.draw_point(corrected_point, (0, 0, 255), 10)
 
                 pygame.display.update()
-                pygame.time.delay(10)
+            pygame.time.delay(10)
 
                 # print(polar_to_screen_coordinates(face.prediction))
 
@@ -122,17 +140,29 @@ class Demo:
                         pygame.quit()
                         return
 
-    def run_calibration(self, name):
-        print("[ CALIBRATION ] starting calibration ")
+    def calib_routine(self):
 
-        pygame.init()
-
-        screen = pygame.display.set_mode((1792, 1120), pygame.FULLSCREEN)
-
-        calib_count = 0
+        calib_count = 0  #
         results = dict()
+        ref_points = [(1792 // 2, 1120 // 2),
+                      (1792 // 5, (1120 // 5) * 2),
+                      (1792 // 5, (1120 // 5) * 3),
+                      (1792 // 5 * 2, 1120 // 5),
+                      (1792 // 5 * 3, 1120 // 5),
+                      (1792 // 5 * 2, 1120 // 5 * 4),
+                      (1792 // 5 * 3, 1120 // 5 * 4),
+                      (1792 // 5 * 4, (1120 // 5) * 2),
+                      (1792 // 5 * 4, (1120 // 5) * 3)]
+        for ref_point in ref_points:
+            point_name = f"{ref_point[0]}_{ref_point[1]}"
+            results[point_name] = dict()
+            results[point_name]["data"] = []
+            results[point_name]["mean"] = None
+        point_idx = 0
 
         while True:
+            self.visualizer.fill_background((255, 255, 255))
+
             ret, frame = self.camera.get_frame()
             if not ret:
                 continue
@@ -140,80 +170,85 @@ class Demo:
             undistorted = cv2.undistort(frame, self.camera.camera_matrix, self.camera.dist_coefficients)
             faces = self.gaze_estimator.detect_faces(undistorted)
 
-            if len(faces) == 0:
-                continue
+            for face in faces:
+                self.gaze_estimator.estimate_gaze(undistorted, face)
+                print("face center: ", face.center)
+                print("gaze vecotr: ", face.gaze_vector)
 
-            screen.fill((255, 255, 255))
-            pygame.draw.circle(screen, (0, 255, 0), (0, 0), 10)
-            pygame.draw.circle(screen, (0, 255, 0), (1792 // 2, 1120 // 2), 10)
-            pygame.draw.circle(screen, (0, 255, 0), (0, 1120), 10)
-            pygame.draw.circle(screen, (0, 255, 0), (1792, 0), 10)
-            pygame.draw.circle(screen, (0, 255, 0), (1792, 1120), 10)
+                self.visualizer.draw_ref_points(ref_points)
+                #draw the point to look at
+                self.visualizer.draw_point(ref_points[point_idx], (255, 0, 0), 10)
 
-            self.gaze_estimator.estimate_gaze(undistorted, faces[0])
-            print("faces[0] center: ", faces[0].center)
-            print("gaze vecotr: ", faces[0].gaze_vector)
-            print("distnce from camera: ", faces[0].get_distance(self.camera))
-            # faces[0].center[2] = faces[0].get_distance(self.camera)[1]
-            plane_norm = np.array([0, 0, 1])
+                self.visualizer.draw_text("Look at the red marker and press space - "
+                                          "Press 'n' to calibrate next marker - "
+                                          "Press 's' to save calibration data",
+                                          (255, 0, 0),
+                                          (10, 10))
 
-            d = np.dot(plane_norm, faces[0].gaze_vector)
 
-            estimated_point = None
+                estimated_point = self.gaze_to_screen(face)
 
-            if d != 0:
-                p_point = np.array([0, 0, 0])
-                t = np.dot(p_point - faces[0].leye.center * 1000, plane_norm) / d
-                gaze_point = faces[0].leye.center * 1000 + t * faces[0].gaze_vector
-                print(f"Gaze point: {gaze_point}")
-                # Check if gaze_point is within the screen boundaries
-                estimated_point = get_point_on_screen((340, 220), (1792, 1120), gaze_point)
-                print(f"Screen coordinates: {estimated_point}")
-                pygame.draw.circle(screen, (255, 0, 0), estimated_point, 10)
-            else:
-                print("[ CALIBRATION ] Gaze vector is parallel to the screen")
+                self.visualizer.draw_point(estimated_point, (0, 0, 255), 10)
 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
                         pygame.quit()
-                        return
-                    elif event.key == pygame.K_SPACE:
-                        if estimated_point is None:
-                            print("[ CALIBRATION ] No gaze point estimated")
-                            continue
-                        calib_data = dict()
+                        exit()
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_q:
+                            pygame.quit()
+                            exit()
+                        elif event.key == pygame.K_SPACE:
+                            if estimated_point is None:
+                                print("[ CALIBRATION ] No gaze point estimated")
+                                continue
 
-                        calib_data["estimated"] = dict()
-                        calib_data["estimated"]["x"] = int(estimated_point[0])
-                        calib_data["estimated"]["y"] = int(estimated_point[1])
-                        calib_data["actual"] = dict()
-                        calib_data["actual"]["x"] = 1792 // 2
-                        calib_data["actual"]["y"] = 1120 // 2
-                        results[calib_count] = calib_data
-                        calib_count += 1
-                        pygame.draw.circle(screen, (0, 255, 0), estimated_point, 10)
-                    elif event.key == pygame.K_s:
-                        with open(f"data/calib/{name}.yaml", "w") as f:
-                            yaml.dump(results, f)
-                        print("[ CALIBRATION ] Calibration data saved")
-                        return
+                            ref_point = ref_points[point_idx]
+                            point_name = f"{ref_point[0]}_{ref_point[1]}"
+                            results[point_name]["data"].append(estimated_point)
 
-            pygame.display.update()
+                            self.visualizer.draw_point(estimated_point, (255, 255, 0), 10)
+                        elif event.key == pygame.K_s and self.mode == 0:
+                            pygame.quit()
+                            return results
+                        elif event.key == pygame.K_n:
+                            if point_idx < len(ref_points) - 1:
+                                point_idx += 1
+                            else:
+                                point_idx = 0
+
+
+                pygame.display.update()
             pygame.time.delay(10)
 
-    def get_calib_data(self, name):
-        with open(f"data/calib/{name}.yaml", "r") as f:
-            data = yaml.load(f, Loader=yaml.FullLoader)
-        avg_x = 0
-        avg_y = 0
-        for key, value in data.items():
-            avg_x += value["estimated"]["x"]
-            avg_y += value["estimated"]["y"]
-        avg_x /= len(data)
-        avg_y /= len(data)
 
-        return (avg_x, avg_y), data
+    def gaze_to_screen(self, face):
+        plane_norm = np.array([0, 0, 1])
+
+        d = np.dot(plane_norm, face.gaze_vector)
+
+        face.center[2] = face.center[2] - 0.0009
+
+        if d != 0:
+            p_point = np.array([0, 0, 0])
+            t = np.dot(p_point - face.leye.center * 1000, plane_norm) / d
+            gaze_point = face.leye.center * 1000 + t * face.gaze_vector
+            print(f"Gaze point: {gaze_point}")
+            # Check if gaze_point is within the screen boundaries
+            x, y = get_point_on_screen((340, 220), (1792, 1120), gaze_point)
+            print(f"Screen coordinates: {x}, {y}")
+
+            return x, y
+        else:
+            print("Gaze vector is parallel to the screen")
+            return None, None
+
+
+
+    def get_calib_data(self, name):
+        with open(f"data/calib/{name}.json", "r") as f:
+            data = json.load(f)
+        return data
 
 
 
@@ -226,6 +261,4 @@ if __name__ == '__main__':
     args = argparse.parse_args()
     mode = args.mode
     demo = Demo(mode, args.user_calib)
-    print("[ Starting demo ]")
-    demo.run()
-    print("[ Demo stopped ]")
+    print("[ MAIN ] program end")
---
+++ utils/visualizer.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Visualizer:
+
+    def __init__(self, screen=None):  # constructor for tracker/calib mode
+        self.screen = screen
+        self.font = pygame.font.SysFont("Arial", 30)
+
+
+    def fill_background(self, color_or_img):
+        self.screen.fill(color_or_img)
+
+    def draw_point(self, point, color, radius):
+        pygame.draw.circle(self.screen, color, point, radius)
+
+    def draw_text(self, text, color, pos):
+        self.screen.blit(self.font.render(text, True, color), pos)
+
+    def draw_ref_points(self, ref_points):
+        for point in ref_points:
+            self.draw_point(point, (0, 255, 0), 10)
+
+
+
+
+
+
